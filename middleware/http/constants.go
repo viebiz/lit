04680f4b180d7a5ee83e193c0a6673cbf0a23d23
the-witcher-knight <dhuuloc8818@gmail.com>
2025-03-05 08:47:30 +0700

    Fix CWE-312 issue

--- middleware/http/constants.go
+++ middleware/http/constants.go
@@ -1,11 +1,8 @@
 package http
 
 // Exported values to allow easy customization of HTTP response headers in the future
-var (
-	// RequestIDHeaderName represents x-request-id key response header
-	RequestIDHeaderName = "x-request-id"
-)
-
 const (
-	httpRequestIDKey string = "http.request.id"
+	// headerXRequestID represents x-request-id key response header
+	headerXRequestID = "x-request-id"
+	httpRequestIDKey = "http.request.id"
 )
--- middleware/http/middleware_request_id.go
+++ middleware/http/middleware_request_id.go
@@ -1,8 +1,15 @@
 package http
 
 import (
+	"fmt"
+	"hash/fnv"
+
 	"github.com/viebiz/lit"
 	"github.com/viebiz/lit/monitoring"
+)
+
+var (
+	hash64Func = hash64
 )
 
 // RequestIDMiddleware ensures each request has a unique Request ID.
@@ -11,22 +18,31 @@ import (
 func RequestIDMiddleware() lit.HandlerFunc {
 	return func(c lit.Context) {
 		// Get request ID from header, if it not exists, generate a new one
-		requestID := c.Request().Header.Get(RequestIDHeaderName)
+		requestID := c.Request().Header.Get(headerXRequestID)
 		if requestID == "" {
 			requestID = idFunc()
 		}
 
 		// Inject request ID to request context
 		ctx := c.Request().Context()
-		ctx = monitoring.InjectField(ctx, httpRequestIDKey, requestID)
+		ctx = monitoring.InjectField(ctx, httpRequestIDKey, hash64Func(requestID))
 
 		// Update the request context
 		c.SetRequestContext(ctx)
 
-		// Add request ID to response header
-		c.Header(RequestIDHeaderName, requestID)
-
 		// Continue handle request
 		c.Next()
+
+		// Add request ID to response header
+		c.Writer().Header().Add(headerXRequestID, requestID)
 	}
+}
+
+func hash64(plain string) string {
+	h := fnv.New64a()
+	if _, err := h.Write([]byte(plain)); err != nil {
+		return plain
+	}
+
+	return fmt.Sprintf("%x", h.Sum64())
 }
