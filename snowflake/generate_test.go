b62327e93d13e02d64fcb9090c855fb1d83a7e85
the-witcher-knight <dhuuloc8818@gmail.com>
2025-03-12 16:12:47 +0700

    Improve unit test

--- snowflake/generate_test.go
+++ snowflake/generate_test.go
@@ -1,6 +1,7 @@
 package snowflake
 
 import (
+	"errors"
 	"sync"
 	"testing"
 	"time"
@@ -9,41 +10,74 @@ import (
 )
 
 func TestGenerator_Generate(t *testing.T) {
-	// Given:
-	gen, err := New()
-	require.NoError(t, err)
+	type mockFlakeData struct {
+		useMock bool
+		out     uint64
+		err     error
+	}
+	tcs := map[string]struct {
+		opts          []Option
+		mockFlakeData mockFlakeData
+		expErr        error
+	}{
+		"default generator": {
+			opts: nil,
+		},
+		"generator with options": {
+			opts: []Option{
+				StartTime(time.Date(2021, 9, 1, 0, 0, 0, 0, time.UTC)),
+				MachineID(12345),
+			},
+		},
+		"error from flake": {
+			mockFlakeData: mockFlakeData{
+				useMock: true,
+				err:     errors.New("simulated error"),
+			},
+			expErr: errors.New("snowflake ID generation failed: simulated error"),
+		},
+		"flake return 0": {
+			mockFlakeData: mockFlakeData{
+				useMock: true,
+				out:     0,
+			},
+			expErr: errors.New("snowflake ID is invalid: 0"),
+		},
+	}
 
-	// When:
-	id, err := gen.Generate()
+	for scenario, tc := range tcs {
+		tc := tc
+		t.Run(scenario, func(t *testing.T) {
+			t.Parallel()
 
-	// Then:
-	require.NoError(t, err)
-	require.NotZero(t, id)
+			// Given:
+			gen, err := New(tc.opts...)
+			require.NoError(t, err)
 
-	// When:
-	id, err = gen.Generate()
+			if tc.mockFlakeData.useMock {
+				mockIDProvider := NewMockidProvider(t)
+				mockIDProvider.
+					EXPECT().
+					NextID().
+					Return(tc.mockFlakeData.out, tc.mockFlakeData.err)
+				gen.flake = mockIDProvider
+			}
 
-	// Then:
-	require.NoError(t, err)
-	require.NotZero(t, id)
+			// When:
+			if tc.expErr != nil {
+				_, err := gen.Generate()
+				require.EqualError(t, err, tc.expErr.Error())
+			} else {
+				id, err := gen.Generate()
+				require.NoError(t, err)
+				require.NotZero(t, id)
 
-	// Given:
-	gen, err = New(StartTime(time.Date(2021, 9, 1, 0, 0, 0, 0, time.UTC)), MachineID(12345))
-	require.NoError(t, err)
-
-	// When:
-	id, err = gen.Generate()
-
-	// Then:
-	require.NoError(t, err)
-	require.NotZero(t, id)
-
-	// When:
-	id, err = gen.Generate()
-
-	// Then:
-	require.NoError(t, err)
-	require.NotZero(t, id)
+				id, err = gen.Generate()
+				require.NoError(t, err)
+				require.NotZero(t, id)
+			}
+		})
+	}
 }
 
 func TestGenerator_Generate_CheckUnique(t *testing.T) {
